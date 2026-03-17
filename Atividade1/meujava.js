018dc1c78b85848907339cc41ee0a2303bc9ef2a
DougWell07 <douglas.wellington07@gmail.com>
2026-03-16 22:21:40 -0300

    Add files via upload

---
+++ Atividade1/meujava.js
@@ -0,0 +1,20 @@
+
+console.log("O que está aconecendo");
+
+let entrada = prompt("Entre com idade");
+console.log("Sua idade é:");
+console.log(entrada);
+
+if(entrada === 9){
+    console.log("ACESSO PERMITIDO");
+}
+else if(entrada > 9){
+    console.log("Você é muito velho");
+}
+else{
+    console.log("ACESSO NEGADO");
+}
+
+for(let i=0; i < 10; i++){
+    windor.alert(i);
+}
